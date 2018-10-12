9026a1cb7d11f5f30d982cb2ded01ba837d70489
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-10-12 18:30:31 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+var maintenance = false;
+
+
+//template partials
+hbs.registerPartials(__dirname + '/views/partials');
+//init template engine
+app.set('view engine', 'hbs');
+//middleware
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFileSync('server.log', log + '\n');
+  next();
+});
+
+app.use((req, res, next) => {
+  if (maintenance) {
+    res.render('maintenance.hbs');
+  } else {
+      next();
+  }
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+//register hbs helpers
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('toUpper', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  //render template content
+  res.render('index.hbs', {
+    welcome: 'Welcome to our first NodeJS Express Server',
+    title: 'Home'
+  })
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    title: 'About us'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+      errorMessage: 'Error occured',
+      errorCode: 500
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000')
+});
