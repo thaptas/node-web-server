eccea0589c5841a32b80a626c210a6170b4776d1
Tomasz Haptas <tomasz.haptas@rapidcodefactory.com>
2018-10-12 19:51:07 +0200

    Projects page added

--- server.js
+++ server.js
@@ -54,6 +54,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    title: 'Projects'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
       errorMessage: 'Error occured',
